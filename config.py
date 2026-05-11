037e31a0479c3957a9605e5e87977be08fb168ff
rekhasarkar2045-code <rekhasarkar2045@gmail.com>
2026-05-11 14:50:53 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# ─────────────────────────────────────────────
+#  Configuration
+# ─────────────────────────────────────────────
+
+BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"   # 👈 Replace with your BotFather token
+API_URL   = "https://tg-number-api-wbka.vercel.app/"
