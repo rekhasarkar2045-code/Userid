f1f8967fed3503ba8e73ee25d651da10a2a92a90
rekhasarkar2045-code <rekhasarkar2045@gmail.com>
2026-05-11 15:04:57 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-# ─────────────────────────────────────────────
-#  Configuration
-# ─────────────────────────────────────────────
-
-BOT_TOKEN = "8604572355:AAHplc24nwqf8frKRV6-TJyZJcxGpy1Zbyg"   # 👈 Replace with your BotFather token
-API_URL   = "https://tg-number-api-wbka.vercel.app/"
